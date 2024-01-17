a70f3241b0b4fc2cebf6cf1996ab43261866d6fd
刘兴民(Xingmin Liu) <liuxingmin@mgi-tech.com>
2024-01-17 10:16:39 +0800

    add slurm config using CookieCutter

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+###################
+## create config directory that snakemake searches for profiles (or use something else)
+# profile_dir="${HOME}/.config/snakemake"
+# mkdir -p "$profile_dir"
+# # use cookiecutter to create the profile in the config directory
+# template="gh:Snakemake-Profiles/slurm"
+# cookiecutter --output-dir "$profile_dir" "$template"
+
 nohup snakemake -p --use-singularity --singularity-args " --fakeroot --bind $PWD:/root --bind /HD101TB/bioinfo/liuxingmin/Clinical/WGS_cnv/clinsv/:/root/clinsv/ " --rerun-incomplete --profile slurm --jobs 28 -s /HD101TB/bioinfo/liuxingmin/Clinical/snk/wgs_clin_v1.0.0.rules.py &
 snakemake --unlock ./ -s /HD101TB/bioinfo/liuxingmin/Clinical/snk/wgs_matepair_v1.0.0.rules.py
 snakemake -s /HD101TB/bioinfo/liuxingmin/Clinical/snk/wgs_clin_v1.0.0.rules.py --rulegraph | dot -Tpng > wgs_clin.rules.png
